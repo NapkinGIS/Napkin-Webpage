1505bbf6618bb00e1478f7fc28111bdbf028409d
AndreasAtakan <andreas@napkingis.no>
2020-08-13 00:07:42 +0200

    Updates to about.html page and small fixes

--- public/js/map.js
+++ public/js/map.js
@@ -20,10 +20,8 @@
 
 "use strict";
 
-let center = [60.381418, 5.326884];
-
 let map = L.map("map", {
-  center: [60.381609, 5.327975],
+  center: [ 60.381609, 5.327975 ],
   zoom: 16
 });
 
@@ -34,8 +32,17 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 L.marker([60.381109, 5.327975]).addTo(map)
   .bindPopup(`
     <h5>Napkin AS</h5>
-    <span style=\"display: block; margin-top: 4px;\"><b>Office:</b> Thormøhlens Gate 41, 5006 Bergen</span>
-    <span style=\"display: block; margin-top: 4px;\"><b>Phone:</b> +47 48 00 63 25</span>
-    <span style=\"display: block; margin-top: 4px;\"><b>E-mail:</b> contact@napkingis.no</span>
+
+    <span style=\"display: block; margin-top: 4px;\">
+      <b>Office:</b> Thormøhlens Gate 41, 5006 Bergen
+    </span>
+
+    <span style=\"display: block; margin-top: 4px;\">
+      <b>Phone:</b> +47 48 00 63 25
+    </span>
+
+    <span style=\"display: block; margin-top: 4px;\">
+      <b>E-mail:</b> contact@napkingis.no
+    </span>
   `)
   .openPopup();
